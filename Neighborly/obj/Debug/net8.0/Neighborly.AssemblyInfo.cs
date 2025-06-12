3ab736d71ad30e7e5a06906868bf3f5283b1a97e
Marcel Rakowski <marcel.rakowski@wp.pl>
2025-06-12 13:03:37 +0200

    upgraded my account

--- Neighborly/obj/Debug/net8.0/Neighborly.AssemblyInfo.cs
+++ Neighborly/obj/Debug/net8.0/Neighborly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Neighborly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7909c3a1a080b3fd75599750873958b408934029")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab62c9002e5d05d4aa70afaa992a501714933f95")]
 [assembly: System.Reflection.AssemblyProductAttribute("Neighborly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Neighborly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
