a5147094c77fec34dc354fc1a7839128053fa682
Adam <kollatajadam@gmail.com>
2025-06-13 13:46:15 +0200

    dodanie pelnej funkcjonalnosci przyciskow do logowania sie rejestracji w lnadingpage i ich znikania

--- Neighborly/obj/Debug/net8.0/Neighborly.AssemblyInfo.cs
+++ Neighborly/obj/Debug/net8.0/Neighborly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Neighborly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02c6ca5c5ed758c6b3fb6ae7b18df25754f2b861")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833cdcdae7e63e2ca3aec6c2b3081532e81766e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Neighborly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Neighborly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
